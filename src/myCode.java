5831b0cae460cbfffd7c75c5a00ed7e74686b4f8
atiya <atiyaintharit38@gmail.com>
2018-06-26 11:40:14 +0700

    Hello World

---
+++ src/myCode.java
@@ -0,0 +1,18 @@
+package leb1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World.");
+        System.out.println("Atiya Intharit");
+        System.out.println("361211760024");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+    }//main
+
+    }//class
+
